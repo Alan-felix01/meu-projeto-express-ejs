ac4e5e59f1a1bdcab0576820176c4f05559cbe4b
Alan-felix01 <alanfelixteles376@gmail.com>
2023-10-26 16:48:34 -0400

    melhora do css

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+const produtos = [
+  {id:376,titulo: "nome do produto",preço:"R$1000",descricao:"descrição do produto",imagem: "camisa.jpg"},
+  {id:376,titulo: "nome do produto",preço:"R$1000",descricao:"descrição do produto",imagem: "camisa.jpg"},
+]
+
 app.get('/', (req, res) => {
   res.render('index', { message: 'Olá, Mundo!' });
+});
+
+app.get('/produtos', (req, res) => {
+  res.render('produtos', { message: 'Olá, Mundo!' });
 });
 
 app.listen(port, () => {
