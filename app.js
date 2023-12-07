ca9f6a4e84d0221a385b11d81a4ec43c951384b4
Alan-felix01 <alanfelixteles376@gmail.com>
2023-12-07 16:40:46 -0400

    07/12/2023
    data a entrevista

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 const produtos = [
-  {id:1,titulo: "Camisa de Preta",preco:"R$1000",descricao:"descrição do produto",imagem: "camisa.jpg"},
-  {id:2,titulo: "Caneleira Preta",preco:"R$1000",descricao:"descrição do produto",imagem: "CANALEIRA PRETA 01.png"},
-  {id:3,titulo: "Caneleira Branca",preco:"R$1000",descricao:"descrição do produto",imagem: "CANELEIRA BRANCA 01.png"},
-  {id:4,titulo: "Kit Faixas",preco:"R$1000",descricao:"descrição do produto",imagem: "kit fAIXAS.jpg"},
-  {id:5,titulo: "Kit Kimono Azul",preco:"R$1000",descricao:"descrição do produto",imagem: "kit kimono azul.jpg"},
-  {id:6,titulo: "Kit Kimono Branco",preco:"R$1000",descricao:"descrição do produto",imagem: "kit kimono branco.jpg"},
-  {id:7,titulo: "Luva Branca 01",preco:"R$1000",descricao:"descrição do produto",imagem: "LUVA BRANCA 01.jpg"},
-  {id:8,titulo: "Luva Branca 02",preco:"R$1000",descricao:"descrição do produto",imagem: "LUVA BRANCA 02.png"},
-  {id:9,titulo: "Luva Preta 01",preco:"R$1000",descricao:"descrição do produto",imagem: "LUVA PRETA 03.jpg"},
-  {id:10,titulo: "Luva Preta 02",preco:"R$1000",descricao:"descrição do produto",imagem: "LUVA PRETA02.png"},
-  {id:11,titulo: "Luva Preta 03",preco:"R$1000",descricao:"descrição do produto",imagem: "LUVA PRETA01.jpg"},
-  {id:12,titulo: "Luva Preta 04",preco:"R$1000",descricao:"descrição do produto",imagem: "LUVA PRETO 04.png"},
+  {id:1,titulo: "Camisa",preco:"R$1000",descricao:"camisa de compressão usa por atletas de baixo e alto nivel.",imagem: "camisa.jpg"},
+  {id:2,titulo: "Caneleira Preta",preco:"R$1000",descricao:"caneleira usa como proteção para artes como kickboxing e muay thay",imagem: "CANALEIRA PRETA 01.png"},
+  {id:3,titulo: "Caneleira Branca",preco:"R$1000",descricao:"caneleira usa como proteção para artes como kickboxing e muay thay",imagem: "CANELEIRA BRANCA 01.png"},
+  {id:4,titulo: "Kit Faixas",preco:"R$1000",descricao:"faixas para artes como karate,jiu jitsu e judo",imagem: "kit fAIXAS.jpg"},
+  {id:5,titulo: "Kit Kimono Azul",preco:"R$1000",descricao:"kimono para judo e jiu jitsu",imagem: "kit kimono azul.jpg"},
+  {id:6,titulo: "Kit Kimono Branco",preco:"R$1000",descricao:"kimono para judo e jiu jitsu",imagem: "kit kimono branco.jpg"},
+  {id:7,titulo: "Luva Branca 01",preco:"R$1000",descricao:"luva para boxe e kickboxing",imagem: "LUVA BRANCA 01.jpg"},
+  {id:8,titulo: "Luva Branca 02",preco:"R$1000",descricao:"luva para boxe e kickboxing",imagem: "LUVA BRANCA 02.png"},
+  {id:9,titulo: "Luva Preta 01",preco:"R$1000",descricao:"luva para boxe e kickboxing",imagem: "LUVA PRETA 03.jpg"},
+  {id:10,titulo: "Luva Preta 02",preco:"R$1000",descricao:"luva para boxe e kickboxing",imagem: "LUVA PRETA02.png"},
+  {id:11,titulo: "Luva Preta 03",preco:"R$1000",descricao:"luva para boxe e kickboxing",imagem: "LUVA PRETA01.jpg"},
+  {id:12,titulo: "Luva Preta 04",preco:"R$1000",descricao:"luva para boxe e kickboxing",imagem: "LUVA PRETO 04.png"},
   
 ]
 
